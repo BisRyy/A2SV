84683f31fffc7a8ebdff3aaa670186172555858a
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2022-12-24 12:08:56 +0300

    Time: 38 ms (91.13%), Space: 13.9 MB (38.38%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        for list_index in range(len(words)-1):
+            index = 0
+            current = words[list_index]
+            next = words[list_index + 1]
+            min_length = min(len(current), len(next))
+            while(index  < min_length):
+                if order.find(current[index]) > order.find(next[index]):
+                    print(1)
+                    return False
+                elif order.find(current[index]) == order.find(next[index]):
+                    if index == min_length -1 and (len(current) > len(next)):
+                        return False
+                    index +=1
+                    # print( order.find(current[index]), order.find(next[index]))
+                    # if (len(current) <= len(next)):
+                    #     index +=1
+                    # elif index < min(len(current), len(next)):
+                    #     index +=1
+                    # else:
+                    #     return False
+                else:
+                    print(3)
+                    break
+        return True
