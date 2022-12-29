5990d84404405d380d0bf35072c2ab2d67b2bd03
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2022-12-29 00:10:47 -0900

    Create 905. Sort Array By Parity.py

---
+++ 905. Sort Array By Parity.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        ans = deque()
+        for num in nums:
+            if num % 2 == 0:
+                ans.appendleft(num)
+            else:
+                ans.append(num)
+                
+        return ans
