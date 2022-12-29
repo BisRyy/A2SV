c2630bb030c2f724947f3226f8b7cdcb67e52f92
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2022-12-29 00:30:52 -0900

    Create set union .py

---
+++ set union .py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+a = set(input().split())
+
+m = int(input())
+b = set(input().split())
+
+count =  a | b
+print(len(count))
