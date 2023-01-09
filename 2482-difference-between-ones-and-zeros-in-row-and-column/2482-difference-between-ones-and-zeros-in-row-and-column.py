7980a034a7b6d5ff6a42eab56b7b02f2ec7bffaf
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-01-09 21:41:28 +0300

    Time: 1793 ms (89.20%), Space: 41.4 MB (84.92%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,18 +1,41 @@
-
+# class Solution:
+#     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
+#         m = len(grid)
+#         n = len(grid[0])
+#         ones = [[],[]] # row, column
+        
+#         for column in range(n):
+#             _sum = 0
+#             for row in range(m):
+#                 # ones[0].append(sum(grid[row]))
+#                 _sum += grid[row][column]
+#             ones[1].append(_sum)
+            
+        
+#         for row in range(m):
+#             ones[0].append(sum(grid[row]))
+            
+#         for row in ones[0]:
+#             temp = []
+#             for col in ones[1]:
+#                 temp.append(row*2 + col - n - m + col *2)
+#             grid[row] = temp
+#         return grid
+        
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
-        # ans = []
-        onesrow,onescol = [],[]
+        ones = [[],[]] # row, column
         for i in range(len(grid[0])):
             onesCol = 0
             for j in range(len(grid)):
                 onesCol += 1 if grid[j][i] else 0
-            onescol.append(onesCol)
+            ones[1].append(onesCol)
+            
         for row in grid:
-            onesrow.append(sum(row))
+            ones[0].append(sum(row))
         
-        for i,row in enumerate(onesrow):
-            for j,col in enumerate(onescol):
+        for i,row in enumerate(ones[0]):
+            for j,col in enumerate(ones[1]):
                 grid[i][j] = row + col - (len(grid[0]) - row) - (len(grid) - col)
                 
         return grid
