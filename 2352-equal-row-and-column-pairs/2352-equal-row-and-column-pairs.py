299d7e672a50100bf7049b2331600f98193354fd
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-01-10 09:00:44 +0300

    Time: 2666 ms (10.44%), Space: 18.9 MB (51.86%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,21 +1,27 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         a = []
+        
+        # rotate the colums
         for i in range(len(grid)):
             t = []
             for j in range(len(grid)):
                 t.append(grid[j][i])
             a.append(t)
-        c = 0
-
+        count = 0
+        
+        # compare the arrays
         for i in range(len(a)):
             for j in range(len(grid)):
-                t = False
+                
+                same = False
                 for k in range(len(grid)):
+                    
                     if grid[i][k] == a[j][k]:
-                        t = True
+                        same = True
                     else:
-                        t = False
+                        same = False
                         break
-                if t: c += 1
-        return c
+                if same: 
+                    count += 1
+        return count
