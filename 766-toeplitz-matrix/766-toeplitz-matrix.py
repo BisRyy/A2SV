bd9e75ee81581b5dee6cf3409ad87f0074fcdd71
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-01-13 17:26:10 +0300

    Time: 95 ms (64.36%), Space: 13.9 MB (34.95%) - LeetHub

---
+++ 766-toeplitz-matrix/766-toeplitz-matrix.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        diagonal = defaultdict(set)
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                diagonal[row - col].add(matrix[row][col])
+                if len(diagonal[row - col]) > 1:
+                    return False
+        return True
