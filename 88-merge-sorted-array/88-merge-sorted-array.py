32e1a57f197f4c97fe2a427497e211e754601d80
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-01-26 22:20:11 +0300

    Time: 64 ms (29.40%), Space: 13.8 MB (80.30%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,10 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        left = m - 1
+        right = n - 1
+        current = m + n -1
+
         
-        nums1[m:] = nums2
-        nums1.sort()
-        
-            
-            
-            
+        while right >=0 and left >=0:
+            if nums1[left] <= nums2[right]:
+                nums1[current] = nums2[right]
+                right-=1
+                current-=1
+
+            else:
+                nums1[current] = nums1[left]
+                left-=1
+                current-=1
+
+        # get leftovers
+        while right >=0:
+            nums1[current] = nums2[right]
+            right-=1
+            current-=1
