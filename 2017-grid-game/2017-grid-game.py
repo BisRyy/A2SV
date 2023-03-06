c402913382f618e7248b516681c3b008b2762744
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-03-06 11:27:52 -0300

    Time: 1125 ms (87.06%), Space: 30 MB (70.08%) - LeetHub

---
+++ 2017-grid-game/2017-grid-game.py
@@ -0,0 +1,11 @@
+class Solution:
+   def gridGame(self, grid: List[List[int]]) -> int:
+        top = sum(grid[0])
+        bottom= 0
+        answer = float('inf')
+
+        for grid0, grid1 in zip(grid[0], grid[1]):
+            top -= grid0
+            answer = min(answer, max(top, bottom))
+            bottom += grid1
+        return answer
