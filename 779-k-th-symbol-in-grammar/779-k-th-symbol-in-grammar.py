956f9fba339d197fe98de9dbeb02046472953ac8
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-03-06 21:09:14 -0300

    Time: 23 ms (97.77%), Space: 13.9 MB (11.58%) - LeetHub

---
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.py
@@ -0,0 +1,14 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        
+        def grammar(n, k):
+            if n == 0:
+                return False
+            
+            prevRow = pow(2, n-1)
+            if k >= prevRow:
+                return not grammar(n-1, k % prevRow)
+            else:
+                return grammar(n-1, k % prevRow)
+
+        return 1 if grammar(n-1, k-1) else 0
