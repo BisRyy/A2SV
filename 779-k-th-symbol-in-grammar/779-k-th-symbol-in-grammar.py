0e6a7af3c5a63f5a33855d42347b1f6d6df9eab5
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-03-06 21:10:22 -0300

    Time: 28 ms (86.97%), Space: 13.9 MB (11.58%) - LeetHub

--- 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.py
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.py
@@ -1,14 +1,22 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
         
-        def grammar(n, k):
-            if n == 0:
-                return False
-            
-            prevRow = pow(2, n-1)
-            if k >= prevRow:
-                return not grammar(n-1, k % prevRow)
+        prev  = self.kthGrammar(n - 1, ceil(k/2))
+        
+        if prev == 1:
+            if k % 2 == 0:
+                return 0
             else:
-                return grammar(n-1, k % prevRow)
-
-        return 1 if grammar(n-1, k-1) else 0
+                return 1
+        else:
+            if k%2 == 0:
+                return 1
+            else:
+                return 0
+                
+                
+            
+            
+        
