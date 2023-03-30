a9fd26bbbe330489812982ff75b58501455c113b
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-03-30 12:31:36 -0300

    Time: 28 ms (81.28%), Space: 13.8 MB (40.50%) - LeetHub

--- 476-number-complement/476-number-complement.py
+++ 476-number-complement/476-number-complement.py
@@ -1,6 +1,6 @@
 class Solution:
     def findComplement(self, num: int) -> int:
         for i in range( num.bit_length()):
-            num = num|(1<<i) if num&(1<<i) == 0 else num&~(1<<i)
+            num ^= (1<<i)
             
         return num
