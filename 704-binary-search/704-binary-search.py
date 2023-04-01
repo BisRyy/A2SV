8c53bb5730714f6c03e3d02df87d3fa5b2f7f3ef
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-04-01 15:17:40 -0300

    Time: 282 ms (5.41%), Space: 15.4 MB (96.81%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,7 +1,4 @@
 class Solution:
     def search(self,nums:List[int],target:int)->int:
-        n=len(nums)
-        for i in range(n):
-            if nums[i]==target:
-                return i
-        return -1 
+        x = bisect_left(nums, target)
+        return x if x < len(nums) and nums[x] == target else -1
