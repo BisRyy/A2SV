66f22106338190bfd71c2b4d80a9a89142a13fd3
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-04-30 20:18:42 +0300

    Time: 57 ms (5.00%), Space: 16.4 MB (6.51%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,26 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        left_start = m-1
-        right_start = n-1
-        up = len(nums1)-1
+        left = m - 1
+        right = n - 1
+        current = m + n -1
 
-        if len(nums1) == n:
-            for i in range(n):
-                nums1[i] = nums2[i]
+        
+        while right >=0 and left >=0:
+            if nums1[left] <= nums2[right]:
+                nums1[current] = nums2[right]
+                right-=1
+                current-=1
 
-        while left_start >= 0 and right_start >= 0:
-            if nums1[left_start] > nums2[right_start]:
-                nums1[up] = nums1[left_start] 
-                left_start-=1
-                up -=1
-            elif nums1[left_start] <= nums2[right_start]:
-                nums1[up] = nums2[right_start] 
-                right_start-=1
-                up -=1
-        while right_start >= 0:
-            nums1[up] = nums2[right_start] 
-            right_start-=1
-            up -=1      
+            else:
+                nums1[current] = nums1[left]
+                left-=1
+                current-=1
 
-            
+        # get leftovers
+        while right >=0:
+            nums1[current] = nums2[right]
+            right-=1
+            current-=1
