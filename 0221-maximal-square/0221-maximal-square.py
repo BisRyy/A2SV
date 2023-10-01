74e4ccd074ef6d3e58cf87c6c515e033056cff57
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-10-01 15:48:38 +0300

    Time: 608 ms (56.16%), Space: 19 MB (96.57%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -2,26 +2,16 @@ class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         r, c = len(matrix), len(matrix[0])
         
-        memo = {}
+        memo = [[0]*(c+1) for i in range(r+1)]
+        ans = 0
         
-        
-        def helper(x, y):
-            if x >= r or y >= c:
-                return 0
-            
-            if (x, y) in memo:
-                return memo[(x, y)]
-            
-            bottom =  helper(x+1, y)
-            right =  helper(x, y+1)            
-            diagonal =  helper(x+1, y+1)    
-            
-            if matrix[x][y] == "1":
-                memo[(x, y)] = 1 + min(bottom, right, diagonal)
-            else:
-                memo[(x,y)] = 0
-            
-            return memo[(x, y)]
-            
-        helper(0,0)
-        return max(memo.values())**2
+        for x in range(r-1, -1, -1):
+            for y in range(c-1, -1, -1):
+                bottom =  memo[x+1][y]
+                right =  memo[x][y+1]           
+                diagonal =  memo[x+1][y+1]    
+
+                if matrix[x][y] == "1":
+                    memo[x][y] = 1 + min(bottom, right, diagonal)
+                ans = max(ans, memo[x][y])
+        return ans**2
