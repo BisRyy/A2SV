c4d96410bd659e76354672944697a9d4dc9d5270
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2023-10-09 11:39:15 +0300

    Time: 76 ms (6.28%), Space: 18 MB (5.93%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -2,27 +2,17 @@ class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         n = len(costs)
         
-        memo = {}
+        @cache
         def dfs(i, a, b):
             if a == b == n // 2:
                 return 0
-            
             if i >= n:
                 return math.inf
-            
             if a > n//2 or b > n // 2:
                 return math.inf
-            
-            if (i, a, b) in memo:
-                return memo[(i,a,b)]
-                
-            x, y = 0, 0
-            
             x =  costs[i][0] + dfs(i+1, a+1, b)
             y =  costs[i][1] + dfs(i+1, a, b+1)
-            
-            memo[(i,a,b)] = min(x, y)
-            return memo[(i,a,b)]
+            return min(x, y)
         
         return dfs(0,0,0)
             
